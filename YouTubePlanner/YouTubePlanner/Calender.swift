d915be1f0dd89eb5f27e972955b25ff03d8a0ab4
AltonRune <2of7.burton@gmail.com>
2019-05-09 13:21:53 -0400

    Calendar added

---
+++ YouTubePlanner/YouTubePlanner/Calender.swift
@@ -0,0 +1,25 @@
+//
+//  Calender.swift
+//  YouTubePlanner
+//
+//  Created by Dungeon Master on 5/9/19.
+//  Copyright © 2019 Dungeon Master. All rights reserved.
+//
+
+import UIKit
+
+class Calender: UIViewController {
+    
+    var calendar =  Calendar.Identifier.gregorian
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    
+}
+
---
+++ YouTubePlanner/YouTubePlanner/List.swift
@@ -0,0 +1,19 @@
+//
+//  List.swift
+//  YouTubePlanner
+//
+//  Created by Dungeon Master on 5/9/19.
+//  Copyright © 2019 Dungeon Master. All rights reserved.
+//
+
+import UIKit
+class List: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
